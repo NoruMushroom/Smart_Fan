3543f1c5ffc9ab261b7e63b8d50dcaa818c22982
NoRu <kimminku98@naver.com>
2022-10-07 01:26:48 +0900

    App

--- Smart Fan/Smart Fan App/app/src/main/java/com/example/smart_fan/MainActivity.java
+++ Smart Fan/Smart Fan App/app/src/main/java/com/example/smart_fan/MainActivity.java
@@ -2,17 +2,48 @@ package com.example.smart_fan;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Switch bluetooth_switch;
+    private int REQUEST_BLUETOOTH_ENABLE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(bluetoothAdapter == null){
+            Toast.makeText(this.getApplicationContext(),"블루투스 사용이 불가능 합니다.",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else{
+            Toast.makeText(this.getApplicationContext(),"블루투스 사용이 가능합니다.",Toast.LENGTH_SHORT).show();
+            if (!bluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, 100);
+            }
+        }//사용 여부 확인 및 권한 얻기
+        bluetooth_switch = findViewById(R.id.bluetooth_btn);
+        bluetooth_switch.setChecked(false);
+        bluetooth_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                
+            }
+        });
     }
 }
