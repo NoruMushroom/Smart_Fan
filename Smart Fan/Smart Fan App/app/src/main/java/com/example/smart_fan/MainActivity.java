30ecd6ecb816c10efcea262845c5bf587924910f
NoRu <kimminku98@naver.com>
2022-10-09 02:41:00 +0900

    App

--- Smart Fan/Smart Fan App/app/src/main/java/com/example/smart_fan/MainActivity.java
+++ Smart Fan/Smart Fan App/app/src/main/java/com/example/smart_fan/MainActivity.java
@@ -11,12 +11,21 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.SeekBar;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Switch bluetooth_switch;
+    private Switch auto_switch;
+    private SeekBar angle_seekbar;
+    private TextView angle_text;
+    private View auto_view;
+    private View angle_view;
     private int REQUEST_BLUETOOTH_ENABLE = 100;
 
     @Override
@@ -25,6 +34,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+        angle_seekbar = findViewById(R.id.angle_bar);
+        angle_text = findViewById(R.id.angle);
+        bluetooth_switch = findViewById(R.id.bluetooth_btn);
+        auto_switch = findViewById(R.id.auto_btn);
+        auto_view = findViewById(R.id.Auto_view);
+        angle_view = findViewById(R.id.Angle_view);
+        auto_view.setVisibility(View.INVISIBLE);//안보이게
+        angle_view.setVisibility(View.INVISIBLE);//안보이게
+        bluetooth_switch.setChecked(false);//초기값은 off
+        auto_switch.setChecked(false);//초기값은 off
         BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if(bluetoothAdapter == null){
             Toast.makeText(this.getApplicationContext(),"블루투스 사용이 불가능 합니다.",Toast.LENGTH_SHORT).show();
@@ -37,13 +56,44 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(enableBtIntent, 100);
             }
         }//사용 여부 확인 및 권한 얻기
-        bluetooth_switch = findViewById(R.id.bluetooth_btn);
-        bluetooth_switch.setChecked(false);
         bluetooth_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                
+                if(isChecked) {
+                    auto_view.setVisibility(View.VISIBLE);
+                }//블루투스 연결했을때
+                else{
+                    auto_switch.setChecked(false);
+                    auto_view.setVisibility(View.INVISIBLE);
+                    angle_view.setVisibility(View.INVISIBLE);
+                }//블루투스 연결을 끊었을때 자동을 수동으로 바꿔주고 바꾼다.
             }
         });
+        auto_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    angle_seekbar.setProgress(90);
+                    angle_view.setVisibility(View.VISIBLE);
+                }//자동일때
+                else{
+                    angle_view.setVisibility(View.INVISIBLE);
+                }//수동일때
+            }
+        });
+        angle_seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                angle_text.setText(progress+"°");
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });//사용자가 원하는 각도에 따라서 값을 출력해준다.
     }
 }
