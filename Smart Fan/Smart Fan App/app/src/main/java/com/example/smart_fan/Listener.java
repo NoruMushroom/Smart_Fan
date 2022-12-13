50bc6587ee9dab14e713401dad33e0d27d4d727b
NoRu <kimminku98@naver.com>
2022-12-13 21:09:55 +0900

    .

--- Smart Fan/Smart Fan App/app/src/main/java/com/example/smart_fan/Listener.java
+++
@@ -1,8 +0,0 @@
-package com.example.smart_fan;
-
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothGatt;
-
-public interface Listener {
-    void Device_info(BluetoothDevice ble_device);
-}
